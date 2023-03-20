ac54b97a2db287d2d26608db00e46c5a3b58b6b6
Tumberjon Rubi <tumberjr@ev-c1.kzn.21-school.ru>
2023-03-20 22:26:21 +0300

    test 2

--- src/yet_another_decision_module/decision.c
+++ src/yet_another_decision_module/decision.c
@@ -1,3 +1,9 @@
+#include "decision.h"
+
+#include <math.h>
+
+#include "../data_libs/data_stat.h"
+
 int make_decision(double *data, int n)
 {
     int decision = 1;
@@ -8,7 +14,7 @@ int make_decision(double *data, int n)
     double min_value = min(data, n);
 
     decision &= (max_value <= m + 3*sigma) &&
-                (max_value >= m - 3*sigma) &&
+                (min_value >= m - 3*sigma) &&
                 (m >= GOLDEN_RATIO);
 
     return decision;
--- src/yet_another_decision_module/yet_another_decision_module_entry.c
+++ src/yet_another_decision_module/yet_another_decision_module_entry.c
@@ -1,10 +1,25 @@
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../data_libs/data_io.h"
+#include "decision.h"
+
+
+int  main()
 {
-    double *data;
+    double *data = NULL;
     int n;
     
+    scanf("%d", &n);
+    data = (double *)calloc(n, sizeof(double));
+
+    input(data,n);
+
     if (make_decision(data, n))
         printf("YES"); 
     else
-        printf("NO");  
+        printf("NO");
+
+    free(data);
+    return 0;  
 }
